06788031e474f0420ebc8de3f37c864acd0430ee
erikgoe <erikgoe@users.noreply.github.com>
2019-05-12 22:58:08 +0200

    Implement the basic game

--- repo/lib/Board.dart
+++ repo/lib/Board.dart
@@ -1,12 +1,101 @@
+import 'dart:math';
+
+import 'package:SimpleDoku/PredefinedBoards.dart';
 
 class Board {
-  var fields = List<List<Field>>();
+  static final int boardBase = 9;
+  static final int boardBasePart = 3;
+  List<List<Field>> fields;
 
-  Board.generate(int seed) {
+  Board.modify(int number, int seed) {
+    clear();
+    var toCopy = PredefinedBoards.boards[number].fields;
+    for (int y = 0; y < Board.boardBase; y++) {
+      for (int x = 0; x < Board.boardBase; x++) {
+        fields[y][x] = Field.of(toCopy[y][x]);
+      }
+    }
+  }
 
+  Board.of(List<Field> fields) {
+    clear();
+    fields.forEach((f) {
+      var field = this.fields[f.y][f.x];
+      field.number = f.number;
+      field.initial = true;
+    });
+  }
+
+  clear() {
+    fields = List<List<Field>>();
+    for (int y = 0; y < boardBase; y++) {
+      var innerList = List<Field>();
+      for (int x = 0; x < boardBase; x++) {
+        innerList.add(Field(x, y));
+      }
+      fields.add(innerList);
+    }
+  }
+
+  bool hasEmpty() {
+    for (int y = 0; y < Board.boardBase; y++)
+      for (int x = 0; x < Board.boardBase; x++)
+        if (fields[y][x].number == null) return true;
+    return false;
+  }
+
+  // returns true when the board has be solved
+  bool checkBoard() {
+    bool ret = true;
+    for (int y = 0; y < Board.boardBase; y++) {
+      for (int x = 0; x < Board.boardBase; x++) {
+        int number = fields[y][x].number;
+        if (number == null) {
+          fields[y][x].valid = true;
+          continue;
+        }
+
+        int countRow = 0, countColumn = 0, countBlock = 0;
+
+        for (int i = 0; i < boardBase; i++) {
+          if (fields[y][i].number == number) countRow++;
+        }
+        for (int i = 0; i < boardBase; i++) {
+          if (fields[i][x].number == number) countColumn++;
+        }
+
+        int blockOffsetX = x ~/ boardBasePart,
+            blockOffsetY = y ~/ boardBasePart;
+        for (int y2 = 0; y2 < boardBasePart; y2++) {
+          for (int x2 = 0; x2 < boardBasePart; x2++) {
+            if (fields[blockOffsetY * boardBasePart + y2]
+                        [blockOffsetX * boardBasePart + x2]
+                    .number ==
+                number) countBlock++;
+          }
+        }
+
+        bool valid = countRow == 1 && countColumn == 1 && countBlock == 1;
+        fields[y][x].valid = valid;
+        if (!valid) ret = false;
+      }
+    }
+    return ret;
   }
 }
 
 class Field {
+  int x, y;
+  int number;
+  bool initial;
+  bool valid = true;
 
+  Field(this.x, this.y, {this.number, this.initial = false});
+  Field.of(Field other) {
+    this.x = other.x;
+    this.y = other.y;
+    this.number = other.number;
+    this.initial = other.initial;
+    this.valid = other.valid;
+  }
 }
--- repo/lib/GameMenu.dart
+++ repo/lib/GameMenu.dart
@@ -1,36 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'Board.dart';
 
 class GameMenu extends StatefulWidget {
-  final double fieldSize = 20;
+  final double fieldSize = 40;
+  final Color borderColor = Colors.grey;
 
   @override
   State<StatefulWidget> createState() => _GameMenuState();
 }
 
 class _GameMenuState extends State<GameMenu> {
-  var board = Board.generate(1);
+  var board = Board.modify(0, 0);
+  Field focussed;
+  bool win = true;
+  bool finished = false;
+
+  void checkBoard() {
+    if (!board.hasEmpty() || finished) {
+      finished = true;
+      if (board.checkBoard()) {
+        setState(() {
+          win = true;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-            children: board.fields
-                .map(
-                  (list) => Row(
-                      children: list
-                          .map(
-                            (field) => Container(
-                                width: widget.fieldSize,
-                                height: widget.fieldSize,
-                                decoration: BoxDecoration(
-                                  border:
-                                      Border.all(color: Colors.grey, width: 1),
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        (focussed != null
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: Wrap(
+                        alignment: WrapAlignment.center,
+                        children: List.generate(
+                            Board.boardBase + 1,
+                            (i) => Padding(
+                                  padding: const EdgeInsets.all(0),
+                                  child: OutlineButton(
+                                    child: (i == Board.boardBase
+                                        ? Icon(Icons.delete)
+                                        : Text((i + 1).toString())),
+                                    onPressed: () {
+                                      setState(() {
+                                        if (i == Board.boardBase)
+                                          focussed.number = null;
+                                        else
+                                          focussed.number = i + 1;
+                                        focussed = null;
+                                        checkBoard();
+                                      });
+                                    },
+                                  ),
                                 )),
-                          )
-                          .toList()),
-                )
-                .toList()));
+                      ),
+                    ),
+                  )
+                ],
+              )
+            : Container()),
+        (win
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Text("Congratulations!",
+                          style: TextStyle(fontSize: 20, color: Colors.green)),
+                    )
+                  ])
+            : Container()),
+        Center(
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: board.fields
+                    .map(
+                      (list) => Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: list
+                              .map(
+                                (field) => Container(
+                                    child: InkWell(
+                                      onTap: () {
+                                        setState(() {
+                                          if (focussed == field ||
+                                              field.initial)
+                                            focussed = null;
+                                          else
+                                            focussed = field;
+                                        });
+                                      },
+                                      child: Center(
+                                          child: Text(
+                                        field.number == null
+                                            ? ""
+                                            : field.number.toString(),
+                                        style: TextStyle(
+                                            color: field.initial
+                                                ? Theme.of(context).primaryColor
+                                                : null),
+                                      )),
+                                    ),
+                                    width: widget.fieldSize,
+                                    height: widget.fieldSize,
+                                    decoration: BoxDecoration(
+                                      color: () {
+                                        if (focussed == field) {
+                                          return Theme.of(context).primaryColor;
+                                        } else {
+                                          if (!field.valid) {
+                                            if (field.initial) {
+                                              return Colors.yellow;
+                                            } else {
+                                              return Colors.red;
+                                            }
+                                          } else
+                                            return Colors.white.withAlpha(50);
+                                        }
+                                      }(),
+                                      border: Border(
+                                          left: BorderSide(
+                                              color: widget.borderColor,
+                                              width: field.x % 3 == 0 ? 2 : 0),
+                                          right: BorderSide(
+                                              color: widget.borderColor,
+                                              width:
+                                                  field.x == Board.boardBase - 1
+                                                      ? 2
+                                                      : 0),
+                                          top: BorderSide(
+                                              color: widget.borderColor,
+                                              width: field.y % 3 == 0 ? 2 : 0),
+                                          bottom: BorderSide(
+                                              color: widget.borderColor,
+                                              width:
+                                                  field.y == Board.boardBase - 1
+                                                      ? 2
+                                                      : 0)),
+                                    )),
+                              )
+                              .toList()),
+                    )
+                    .toList())),
+      ]),
+    );
   }
 }
